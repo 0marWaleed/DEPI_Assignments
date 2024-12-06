287d8913e79e15524cd7e400d345da4acd28338c
Omar Waleed <omar101waleed@gmail.com>
2024-12-06 19:22:17 +0200

    Add files via upload

---
+++ Assignment_5.sql
@@ -0,0 +1,36 @@
+--Q1-1
+select * from employees
+
+--Q1-2
+select e.employee_id, e.first_name, e.last_name, d.department_id from employees e, departments d where e.department_id = d.department_id
+union 
+select null, city, null, null from locations where city = 'Cairo';
+
+--Q2-1
+select distinct department_id from employees
+
+--Q3
+create table students (
+    ID int identity(1,1) primary key,
+    first_name varchar(50) not null,
+    last_name varchar(50) default 'Unknown',
+    address varchar(100) default 'N/A',
+    city varchar(50) default 'N/A',
+    birth_date date
+);
+
+--Q3-2
+drop table students
+
+--Q4-1
+insert into students values ('Ahmed', 'Ali', 'Downtown', 'Cairo', '1995-01-01')
+select * from students
+
+--Q4-2
+update students
+set address = 'Cairo' where last_name = 'Ahmed';
+
+--Q5
+begin transaction
+delete from students where city = 'Cairo'
+rollback transaction 
---
+++ Assignment_6.sql
@@ -0,0 +1,13 @@
+--Task 1
+select employee_id, last_name, salary from employees
+where salary between 2000 and 5000 and manager_id not in (101, 200);
+
+
+--Task 2
+select e.first_name,e.last_name,d.department_name
+from employees e inner join departments d on e.department_id = d.department_id order by d.department_name asc;
+
+
+--Task 3
+select e.department_id, count(e.employee_id) as employee_id, avg(e.salary) as average_salary
+from employees e group by e.department_id; 
